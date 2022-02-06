04ecea8b672920aa560baa00e4199a6867b91573
Vladislav Yaromchik <vladislav.yaromchik@gmail.com>
2022-02-07 01:38:18 +0300

    fix: incorrect display of default currency

--- app/src/main/java/com/yaromchikv/moneymanager/feature/presentation/utils/Utils.kt
+++ app/src/main/java/com/yaromchikv/moneymanager/feature/presentation/utils/Utils.kt
@@ -14,7 +14,7 @@ import com.yaromchikv.moneymanager.R
 
 object Utils {
     const val MAIN_COLOR = "#ffee423e"
-    const val MAIN_CURRENCY = "BYN"
+    const val MAIN_CURRENCY = "USD"
 
     const val THEME_PREFERENCE_KEY = "theme"
     const val THEME_DARK = "dark"
